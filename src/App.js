b5a813e5767f1f94328ed66e064ffb4af86a3875
EstebanCarrilloG <estexav0996@gmail.com>
2024-11-11 19:55:36 -0500

    drum machine done * all test passed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import sounds from "./data/sounds";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [displayName, setDisplayName] = useState("");
+  const playSound = (key, name) => {
+    setDisplayName(name);
+    const audio = document.getElementById(key);
+    audio.currentTime = 0;
+    audio.play();
+  };
+
+  window.addEventListener("keydown", (event) => {
+    const key = event.key.toUpperCase();
+
+    if (
+      key === "Q" ||
+      key === "W" ||
+      key === "E" ||
+      key === "A" ||
+      key === "S" ||
+      key === "D" ||
+      key === "Z" ||
+      key === "X" ||
+      key === "C"
+    ) {
+      const audio = document.getElementById(key);
+
+      audio.parentElement.classList.add("active");
+      setDisplayName(audio.attributes.name.value);
+      setTimeout(() => {
+        audio.parentElement.classList.remove("active");
+      }, 200);
+      audio.currentTime = 0;
+      audio.play();
+    }
+
+    return;
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <div id="drum-machine">
+          <h1>Drum Machine</h1>
+          <div className="pad-bank-and-display">
+            <div id="display">{displayName}</div>
+
+            <div className="pad-bank">
+              {sounds.map((sound) => (
+                <div
+                  key={sound.name}
+                  className="drum-pad"
+                  id={sound.name}
+                  onClick={() => playSound(sound.key, sound.name)}
+                >
+                  <audio
+                    className="clip"
+                    name={sound.name}
+                    id={sound.key}
+                    src={sound.url}
+                  ></audio>
+                  {sound.key}
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/data/sounds.js
@@ -0,0 +1,61 @@
+import heater1 from "../sounds/Heater-1.mp3"
+import heater2 from "../sounds/Heater-2.mp3"
+import heater3 from "../sounds/Heater-3.mp3"
+import heater4 from "../sounds/Heater-4_1.mp3"
+import clap from "../sounds/Heater-6.mp3"
+import openHh from "../sounds/Dsc_Oh.mp3"
+import kickNHat from "../sounds/Kick_n_Hat.mp3"
+import kick from "../sounds/RP4_KICK_1.mp3"
+import closedHh from "../sounds/Cev_H2.mp3"
+
+
+
+const sounds = [
+    {
+        name:"Heater 1",
+        url:heater1,
+        key:"Q"
+    },
+    {
+        name:"Heater 2",
+        url:heater2,
+        key:"W"
+    },
+    {
+        name:"Heater 3",
+        url:heater3,
+        key:"E"
+    },
+    {
+        name:"Heater 4",
+        url:heater4,
+        key:"A"
+    },
+    {
+        name:"Clap",
+        url:clap,
+        key:"S"
+    },
+    {
+        name:"Open HH",
+        url:openHh,
+        key:"D"
+    },{
+        name:"Kick n' Hat",
+        url:kickNHat,
+        key:"Z"
+
+    },
+    {
+        name:"Kick",
+        url:kick,
+        key:"X"
+    },
+    {
+        name:"Closed HH",
+        url:closedHh,
+        key:"C"
+    }
+]
+
+export default sounds
